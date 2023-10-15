79cd37c653cc5d27bec46a1b6de5ec53f81e9482
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-15 19:23:39 +0800

    Update

---
+++ Based/GL_RenderPipeline.cpp
@@ -0,0 +1,21 @@
+#include "GL_RenderPipeline.h"
+
+
+namespace OpenGLSamples::Based {
+
+	void GL_RenderPipeline::init()
+	{
+
+	}
+
+	void GL_RenderPipeline::render()
+	{
+
+	}
+
+	void GL_RenderPipeline::close()
+	{
+
+	}
+
+}
---
+++ Based/GL_RenderPipeline.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "RenderPipeline.h"
+
+
+namespace OpenGLSamples {
+	namespace Based {
+
+		class GL_RenderPipeline : public RenderPipeline
+		{
+		public:
+			void init();
+
+			void render();
+
+			void close();
+
+		private:
+
+		};
+
+	}
+}
