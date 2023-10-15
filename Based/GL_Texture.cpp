94d05f64138462ea43f6b61b945de6dc40e83a1d
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-16 00:12:54 +0800

    Update

--- Based/GL_Texture.cpp
+++ Based/GL_Texture.cpp
@@ -1,3 +1,7 @@
+#include <glad/glad.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 #include "GL_Texture.h"
 
 
@@ -5,14 +9,46 @@ namespace OpenGLSamples::Based {
 
 	bool GL_Texture::init()
 	{
+		if (filePath.empty()) {
+			std::cout << "Failed get texture" << std::endl;
+			return false;
+		}
 
+		glGenTextures(1, &handle);
+		glBindTexture(GL_TEXTURE_2D, handle);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// set texture filtering parameters
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		int width, height, nrChannels;
+		stbi_set_flip_vertically_on_load(true);
+
+		unsigned char* data = stbi_load(filePath.c_str(), &width, &height, &nrChannels, 0);
+		if (data) {
+			if (nrChannels == 3) {
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			}
+			if (nrChannels == 4) {
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+			}
+
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else {
+			std::cout << "Failed to load texture" << std::endl;
+			return false;
+		}
+		stbi_image_free(data);
 
 		return true;
 	}
 
 	void GL_Texture::use()
 	{
-
+		glBindTexture(GL_TEXTURE_2D, handle);
 	}
 
 	void GL_Texture::close()
