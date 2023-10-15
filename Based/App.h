90c5ae7e6d8a95bb999da82b162fab4ceb765158
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-15 17:39:11 +0800

    Update

--- Based/App.h
+++ Based/App.h
@@ -1,11 +1,31 @@
 #pragma once
+#include <iostream>
+#include "Type.h"
 
-class App
-{
-public:
-	App();
-	~App();
+using namespace std;
 
-private:
+namespace OpenGLSamples {
+	namespace Based {
 
-};
+		//所有应用的抽象类
+		class App {
+
+		public:
+			Type::win_info_s info;
+
+		public:
+			App();
+			~App();
+
+			bool init(int _width, int _height, string _title);
+
+			void run();
+
+			void close();
+
+		private:
+
+		};
+
+	}
+}
---
+++ Based/Type.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+namespace OpenGLSamples {
+	namespace Type {
+
+		//´°¿ÚÐÅÏ¢
+		struct win_info_s {
+			int width, height;
+			string title;
+		};
+
+	}
+}
