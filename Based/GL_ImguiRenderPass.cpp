4676fd52cf8abe676764c5eef776662f61eb0008
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-17 14:57:40 +0800

    Update GL_ImguiRenderPass.cpp

--- Based/GL_ImguiRenderPass.cpp
+++ Based/GL_ImguiRenderPass.cpp
@@ -6,6 +6,8 @@
 #include "GL_App.h"
 
 
+ImGuiIO* io = NULL;
+
 namespace OpenGLSamples::Based {
 
 	bool GL_ImguiRenderPass::init()
@@ -13,14 +15,14 @@ namespace OpenGLSamples::Based {
 		/*-------------------设置Imgui-------------------*/
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
-		ImGuiIO& io = ImGui::GetIO();
+		io = &ImGui::GetIO();
 		(void)io;
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+		(*io).ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+		(*io).ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
 		ImGui::StyleColorsDark();
 
-		//ImGui_ImplGlfw_InitForOpenGL(window, true); //会报错，或许是因为GLFW的版本问题？
+		ImGui_ImplGlfw_InitForOpenGL(window, true); //会报错，或许是因为GLFW的版本问题？
 		ImGui_ImplOpenGL3_Init();
 
 		return true;
@@ -28,12 +30,32 @@ namespace OpenGLSamples::Based {
 
 	void GL_ImguiRenderPass::render()
 	{
+		//启动ImGui帧
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
+		//ImGui Begin 到 End
+		{
+			ImGui::Begin("Imgui window");
+
+			ImGui::Text("This is some option");
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / (*io).Framerate, (*io).Framerate);
+
+			ImGui::End();
+		}
+
+		//渲染Imgui
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
 
 	void GL_ImguiRenderPass::close()
 	{
-
+		//关闭ImGui
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
 	}
 
 }
