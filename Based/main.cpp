5313ce33bd361b38f358b3cad636c3f9ceb034f4
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-21 21:50:50 +0800

    Update

--- Based/main.cpp
+++ Based/main.cpp
@@ -11,15 +11,23 @@ using namespace OpenGLSamples::Based;
 int main()
 {
 	GL_World world01;
-	world01.set({
+	world01.set(
 		RendererObject(
 			"..\\assets\\CornellBox.obj",
 			"..\\assets\\CornellBox_Color.png",
 			"..\\assets\\model_vs.glsl", "..\\assets\\model_fs.glsl",
 			glm::vec3(0.0f, 0.0f, 0.0f),
 			glm::vec3(0.0f, 1.0f, 0.0f),
-			glm::vec3(1.0f))
-		});
+			glm::vec3(1.0f)));
+	world01.set(
+		RendererObject(
+			"..\\assets\\巴黎公社墙_卡通_Lod_合并.obj",
+			"..\\assets\\CornellBox_Color.png",
+			"..\\assets\\model_vs.glsl", "..\\assets\\model_fs.glsl",
+			glm::vec3(0.0f, 0.0f, 0.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f),
+			glm::vec3(5.0f))
+	);
 
 	GL_App app;
 
