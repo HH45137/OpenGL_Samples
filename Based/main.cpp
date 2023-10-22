ec3685b959af4a3a149514d005327505c2af3f91
HH45137 <52552476+HH45137@users.noreply.github.com>
2023-10-22 23:29:37 +0800

    Update

--- Based/main.cpp
+++ Based/main.cpp
@@ -21,8 +21,8 @@ int main()
 			glm::vec3(1.0f)));
 	world01.set(
 		RendererObject(
-			"..\\assets\\巴黎公社墙_卡通_Lod_合并.obj",
-			"..\\assets\\巴黎公社墙_卡通_Lod_合并.png",
+			"..\\assets\\PC_Mesh.obj",
+			"..\\assets\\PC_Texture.png",
 			"..\\assets\\model_vs.glsl", "..\\assets\\model_fs.glsl",
 			glm::vec3(0.0f, 0.0f, 0.0f),
 			glm::vec3(0.0f, 1.0f, 0.0f),
