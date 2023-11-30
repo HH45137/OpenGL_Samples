119bed59cfd53091e9cd5476f0dc10cf0cc6038d
Carl-Y <52552476+HH45137@users.noreply.github.com>
2023-11-30 14:58:41 +0800

    Update Camera.h

--- Based/Camera.h
+++ Based/Camera.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#include "RendererObject.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/random.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
+#include "Type.h"
 
 namespace OpenGLSamples::Based {
 
