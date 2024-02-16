a609d26c1e80be7a71a5a15836ab6eef8a4ef85b
Carl-Y <52552476+HH45137@users.noreply.github.com>
2024-02-16 20:34:09 +0800

    Fullscren plane

--- Based/GL_App.cpp
+++ Based/GL_App.cpp
@@ -14,10 +14,10 @@ namespace OpenGLSamples::Based {
 	Shader planeShader;
 	GLuint VAO, VBO, EBO;
 	float vertices[] = {
-		0.5f, 0.5f, 0.0f,   // 右上角
-		0.5f, -0.5f, 0.0f,  // 右下角
-		-0.5f, -0.5f, 0.0f, // 左下角
-		-0.5f, 0.5f, 0.0f   // 左上角
+		1.0f,  1.0f,  0.0f,	// 右上角
+		1.0f,  -1.0f, 0.0f,	// 右下角
+		-1.0f, -1.0f, 0.0f,	// 左下角
+		-1.0f, 1.0f,  0.0f	// 左上角
 	};
 	unsigned int indices[] = {
 		// 注意索引从0开始! 
